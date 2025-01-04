fda01493956b63f3ba4eb5670474d18260ca84a1
Darya Ushakova <ushakovad@yandex.ru>
2025-01-04 17:27:04 +0300

    Add tooltip

--- src/components/Contacts/index.tsx
+++ src/components/Contacts/index.tsx
@@ -1,6 +1,7 @@
 import style from './style.module.css';
 import vk from '../../images/Contacts/VK.svg';
 import instagram from '../../images/Contacts/Instagram.svg';
+import { Tooltip } from 'react-tooltip';
 
 
 const Contact = () => {
@@ -10,14 +11,21 @@ const Contact = () => {
                 <div>
                     <h2 className={style.h2}>Контакты</h2>
                     <div className={style.office}>
-                        <p className={style.name}>ГЛАВНЫЙ ОФИС</p>
+                        <div className={style.block1}>
+                            <p className={style.name}>ГЛАВНЫЙ ОФИС</p>
+                            <button id="tooltip" className={style.qbutton}>?</button>
+                            <Tooltip anchorSelect="#tooltip" className={style.tooltip}>
+                            <h4 className={style.h4}>Адрес и телефон для корреспонденции, инвесторов. Вопросы о доставке, качестве обслуживания и товара просьба задавать в отдел продаж</h4>
+                            </Tooltip>
+
+                        </div>
                         <a href="tel:+78007898989" className={style.tel}>+7 800 789 89 89</a>
-                        <p className={style.adress}>г. Санкт-Петербург, Комсомольская, 43 к1</p>
+                        <p className={style.address}>г. Санкт-Петербург, Комсомольская, 43 к1</p>
                     </div>
                     <div className={style.office}>
                         <p className={style.name}>ОТДЕЛ ПРОДАЖ</p>
                         <a href="tel:+78007898989" className={style.tel}>+7 800 789 89 89</a>
-                        <p className={style.adress}>г. Санкт-Петербург, Комсомольская, 43 к1</p>
+                        <p className={style.address}>г. Санкт-Петербург, Комсомольская, 43 к1</p>
                     </div>
                     <div className={style.icon}>
                         <img className={style.img} src={vk} alt="" />
